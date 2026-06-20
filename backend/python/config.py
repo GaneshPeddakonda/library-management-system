e607673c9b873644c4bb8d33df207bbce3924953
Ganesh Peddakonda <ganeshpeddakonda9@gmail.com>
2026-06-20 21:50:00 +0530

    Update database configuration

--- backend/python/config.py
+++ backend/python/config.py
@@ -3,10 +3,10 @@
 # ============================================================
 import os
 
-DB_HOST = 'localhost'
-DB_USER = 'root'
-DB_PASSWORD = 'Ganesh@9642'
-DB_NAME = 'library_management'
+DB_HOST = 'sql5.freesqldatabase.com'
+DB_USER = 'sql5831052'
+DB_PASSWORD = 'iJRRdBAter'
+DB_NAME = 'sql5831052'
 SECRET_KEY       = os.getenv('SECRET_KEY', 'lms-secret-key-change-in-production')
 FINE_PER_DAY     = 2.00   # ₹ per overdue day
 LOAN_DAYS        = 14     # default loan period
